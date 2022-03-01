2791310a26bb51fb1a4da24088c76c547b750c80
Ouyei <36935397+Ouyei@users.noreply.github.com>
2022-03-01 13:57:13 -0800

    task3

--- models/base_model.py
+++ models/base_model.py
@@ -11,19 +11,17 @@ class BaseModel:
     """Create a BaseModel class"""
     def __init__(self, *args, **kwargs):
         """Public instance attribute"""
-        if kwargs is not None:
-            for key, value in kwargs.items():
-                if key == "__class__":
-                    continue
-                elif key == "created_at" or key == "updated_at":
-                    iso_time = datetime.strptime(value, %Y-%m-%dT%H:%M:%S.%f)
-                    setattr(self, key, iso_time)
-                else:
-                    setattr(self, key, value)
+        if kwargs:
+            for arg, value in kwargs.items():
+                if arg in ('created_at', 'updated_at'):
+                    value = datetime.strptime(value, %Y-%m-%dT%H:%M:%S.%f)
+                if arg != '__class__':
+                    setattr(self, arg, value)
         else:
             self.id = str(uuid.uuid4())
-            self.created_at = datetime.datetime.now()
-            self.updated_at = datetime.datetime.now()
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+            models.storage.new(self)
 
     def __str__(self):
         """Print id and dict"""

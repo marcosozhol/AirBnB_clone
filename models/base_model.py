26c7437e7dfdf4ad91cb36a3d35dbad716dfd8d5
Marcosozhol <3956@holbertonschool.com>
2022-03-01 07:10:05 -0800

    Task 3 - create baseModel class

---
+++ models/base_model.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python3
+"""Base model"""
+import uuid
+import datetime
+
+
+class BaseModel:
+    """Create a BaseModel class"""
+    def __init__(self, *args, **kwargs):
+        """Public instance attribute"""
+        if kwargs is not None:
+            for key, value in kwargs.items():
+                if key == "__class__":
+                    continue
+                elif key == "created_at" or key == "updated_at":
+                    iso_time = datetime.strptime(value, %Y-%m-%dT%H:%M:%S.%f)
+                    setattr(self, key, iso_time)
+                else:
+                    setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.datetime.now()
+            self.updated_at = datetime.datetime.now()
+
+    def __str__(self):
+        """Print id and dict"""
+        return ("[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__))
+
+    def save(self):
+        """
+        Updates the public
+        instance attribute updated_at
+        with the current datetime
+        """
+        self.update_at = datetime.now()
+
+    def to_dict(self):
+        """
+        Returns a dictionary containing
+        all keys/values of __dict__
+        """
+        dict_n = self.__dict__.copy()
+        dict_n.update({"__class__": self.__class__.__name__})
+        dict_n.update({"created_at": self.created_at.isoformat()})
+        dict_n.update({"updated_at": self.updated_at.isoformat()})
+        return (dict_n)
+
+

844d97b02ba78d33882ce9caa2ca9b654c52ad03
Ouyei <36935397+Ouyei@users.noreply.github.com>
2022-03-01 11:17:17 -0800

    variable storage updated

--- models/base_model.py
+++ models/base_model.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
-"""Base model"""
-import uuid
-import datetime
+"""Create class BaseModel that defines all common
+   attributes/methods for other class
+"""
+from uuid import uuid4
+from datetime import datetime
+
 
 
 class BaseModel:
@@ -27,12 +30,13 @@ class BaseModel:
         return ("[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__))
 
     def save(self):
-        """
-        Updates the public
-        instance attribute updated_at
-        with the current datetime
+        """Updates a Base Model instance
+        Updates the public instance attribute `updated_at`
+        with the current datetime and dumps the class data
+        into a file
         """
         self.update_at = datetime.now()
+        models.storage.save()
 
     def to_dict(self):
         """
@@ -44,5 +48,3 @@ class BaseModel:
         dict_n.update({"created_at": self.created_at.isoformat()})
         dict_n.update({"updated_at": self.updated_at.isoformat()})
         return (dict_n)
-
-

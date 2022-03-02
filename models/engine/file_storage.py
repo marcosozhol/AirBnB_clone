97f08410a36f3fdae704c88fdb39f679155f786d
Ouyei <36935397+Ouyei@users.noreply.github.com>
2022-03-02 12:39:59 -0800

    task 5 wroks

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,11 +1,16 @@
 #!/usr/bin/python3
-"""Serializes instances to a JSON file and.
-   deserializes JSON file to instances
+# -*- coding: utf-8 -*-
+"""File Storage Module
+This module is in charge of the storage of the
+classes and their management.
 """
+
 import json
+from models.base_model import BaseModel
+from os import path
 
 
-class FileStorage():
+class FileStorage:
     """File Storage Class
     This is the File Storage module.
     Attributes:
@@ -13,14 +18,14 @@ class FileStorage():
             the contents of the `__objects` variable will be stored.
         __objects (dict): This store all the instances data
     """
-    __file_path = "file.json"
+    __file_path = 'objects.json'
     __objects = {}
 
     def all(self):
         """Gets the __objects info
         Returns the content of the `__objects` class attribute.
         """
-        return FileStorage.__objects
+        return self.__objects
 
     def new(self, obj):
         """Saves a new object in the `__objects` class attribute
@@ -29,8 +34,8 @@ class FileStorage():
         Sets in the `__objects` class attribute the instance data
         with a key as <obj class name>.id.
         """
-        key = "{}.{}".format(obj.__class__.__name__, obj.id)
-        FileStorage.__objects[key] = obj
+        key = obj.__class__.__name__ + '.' + obj.id
+        self.__objects[key] = obj
 
     def save(self):
         """Serializes the content of `__objects` class attribute
@@ -38,12 +43,11 @@ class FileStorage():
         to the path of `__file_path` class attribute in JSON format
         with the `created_at` and `updated_at` formatted.
         """
-
-        new = {}
+        json_dict = {}
+        for key, value in self.__objects.items():
+            json_dict[key] = value.to_dict()
         with open(self.__file_path, mode='w', encoding='utf-8') as my_file:
-            for key, value in FileStorage.__objects.items():
-                new.update({key: value.to_dict()})
-            my_file.write(json.dumps(new))
+            my_file.write(json.dumps(json_dict))
 
     def reload(self):
         """Deserializes the JSON file in `__file_path` class attribute
@@ -51,16 +55,8 @@ class FileStorage():
         on the file will be deserialized and appended to the `__objects`
         class attribute like an instance with the object data.
         """
-        try:
+        if path.exists(self.__file_path):
             with open(self.__file_path, mode='r', encoding='utf-8') as my_file:
-                from models.base_model import BaseModel
-                """here sould continue more models
-                """
-                dict_n = json.loads(my_file.read())
-                for key, value in dict_n.items():
-                    class_name = value.get("__class__")
-                    objt = eval(class_name + "(**value)")
-                    FileStorage.__objects[key] = objt
-
-        except IOError:
-            pass
+                json_dict = json.loads(my_file.read())
+                for key, value in json_dict.items():
+                    self.__objects[key] = eval(value['__class__'])(**value)
